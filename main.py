88a21879555c6c29eded1572f305ecb9d65b6df6
justbegan <justbegan@mail.ru>
2024-03-02 10:41:57 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from pymem import Pymem
+from pymem.process import module_from_name
+
+
+process = 0x4581938
+var = float(input("Distance:",))
+
+
+try:
+    mem = Pymem("dota2.exe")
+    game_module = module_from_name(mem.process_handle, "client.dll").lpBaseOfDll
+    mem.write_float(game_module + process, var)
+    print("Successful")
+except Exception as e:
+    print(f"error {e}")
